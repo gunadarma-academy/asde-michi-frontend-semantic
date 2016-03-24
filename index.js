a8975272f3642abc52fe97ee4a8b3c8658560976
M Haidar Hanif <git@mhaidarhanif.com>
2016-03-25 00:45:51 +0700

    Migrate style.css to global site overrides
    Modify:
    index.html
    index.js
    
    Include:
    .jsbeautifyrc
    white-image.png

---
+++ index.js
@@ -0,0 +1,14 @@
+var main = function () {
+
+  $('.button').click(function () {
+    $('#login').modal('show');
+    $('.landing').dimmer('show');
+  });
+
+}
+
+$(document).ready(main);
+
+// Refer to
+// http://semantic-ui.com/examples/homepage.html
+// http://tcvalentine.com/semantic-themes/minimal-slide/index.html
